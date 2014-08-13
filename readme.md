a30d28edd689c3ec9de24254777f55d7942f5506
crossjs <one@ffully.com>
2014-08-13 15:29:16 +0800

    init

---
+++ readme.md
@@ -0,0 +1,27 @@
+#article
+
+[![Build Status](https://api.travis-ci.org/pandorajs/article.png?branch=master)](http://travis-ci.org/pandorajs/article)
+[![Coverage Status](https://coveralls.io/repos/pandorajs/article/badge.png?branch=master)](https://coveralls.io/r/pandorajs/article?branch=master)
+
+ > article widget, font size etc, seajs module
+
+##how to demo
+
+1. checkout
+1. run `npm install`
+1. run `spm install`
+1. run `grunt`
+1. view files in `/demo`
+
+##how to use
+
+1. run `spm install pandora/article`
+1. write `require('pandora/article/VERSION.NUMBER/article')`
+
+##find examples
+
+1. view the source files in '/src'
+
+##history
+
+- 1.0.0 - release
---
+++ src/article.js
@@ -0,0 +1,100 @@
+define(function(require, exports, module) {
+
+  'use strict';
+
+  var $ = require('$'),
+    Widget = require('widget'),
+    Tips = require('tips');
+
+  var Article = Widget.extend({
+
+    defaults: {
+      container: null,
+      delegates: {
+        'click [data-role=set-fontsize] > a': function(e) {
+          this.setFontSize(e.currentTarget);
+        },
+        'click [data-role=add-bookmark]': function(e) {
+          e.preventDefault();
+          if (window.external && window.external.addFavorite) {
+            window.external.addFavorite(window.location.href, window.title);
+          }else{
+            this.showTips('请按Ctrl+D将本页添加到收藏夹');
+          }
+        }
+      },
+      element: '.article'
+    },
+
+    setup: function() {
+      this.render();
+
+      this.initPagination();
+    },
+
+    setFontSize: function(item) {
+      var fontSize = item.title;
+
+      if ($(item).hasClass('current')) {
+        return;
+      }
+
+      if (!this.fontSizeTarget) {
+        this.fontSizeTarget = this.$('.content, .summary');
+      }
+
+      $(item).addClass('current')
+          .siblings('.current').removeClass('current');
+
+      this.fontSizeTarget.css({
+        'font-size': fontSize + 'px'/*,
+        'line-height': Math.ceil(fontSize * 1.8) + 'px'*/
+      });
+    },
+
+    initPagination: function() {
+      this.pageSwitcher = this.$('.mod-page-switcher');
+      this.pageSwitcher.before('<div class="mod-page-switcher-tip">(支持键盘 &larr; 和 &rarr; 分页)</div>');
+
+      // 添加面包屑
+      this.pageSwitcher.find('a[href]').each(function() {
+        this.href += '#breadcrumb';
+      });
+
+      this.initDelegates({
+        'keyup': function(e) {
+          var href;
+          switch (e.keyCode) {
+            case 37:
+              href = this.$('.page-current').prev().prop('href');
+              if (typeof href !== 'undefined') {
+                window.location = href;
+              } else {
+                this.showTips('当前已经是第一页');
+              }
+              break;
+            case 39:
+              href = this.$('.page-current').next().prop('href');
+              if (typeof href !== 'undefined') {
+                window.location = href;
+              } else {
+                this.showTips('当前已经是最后一页');
+              }
+              break;
+          }
+        }
+      }, this.document);
+    },
+
+    showTips: function(info) {
+      new Tips({
+        content: info,
+        importStyle: true
+      });
+    }
+
+  });
+
+  module.exports = Article;
+
+});
